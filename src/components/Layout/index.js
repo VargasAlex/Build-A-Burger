770baab6238f48afb86cd4c24b4ecad1b3e5c0de
Alex Vargas <alexv2489@gmail.com>
2018-12-21 12:57:00 -0500

    add class & style to layout component

--- src/components/Layout/index.js
+++ src/components/Layout/index.js
@@ -1,11 +1,12 @@
 import React from 'react';
+import './style.css'
 
 import Aux from '../../hoc/Aux';
 
 const layout = (props) => (
   <Aux>
   <div>Toolbar, SideDrawer, Backdrop</div>
-    <main>
+    <main className='Content'>
       {props.children}
     </main>
   </Aux>
